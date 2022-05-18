7aeaf30bda1386695ef3b57ced113bebcf9f3603
Matthew Brown <mjbme10@yahoo.com>
2022-05-17 19:24:16 -0500

    Made changes to "command" and "interpreter"

--- main.tf
+++ main.tf
@@ -88,11 +88,11 @@ resource "aws_instance" "name" {
 
 
   provisioner "local-exec" {
-    command = templatefile("linux-ssh-config.tpl", {
+    command = templatefile("${var.host_os}-ssh-config.tpl", {
       hostname = self.public_ip 
       user = "ubuntu"
       identityfile = "~/.ssh/mykeys/dev_env"
     })
-    interpreter = ["bash", "-c"]
+    interpreter = var.host_os == "linux" ? ["bash", "-c"] : ["Powershell", "-Command"]
   }
 }
