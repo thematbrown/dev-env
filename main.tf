1db7b5851d25df6b31b6eafb673277728cf69dd8
Matthew Brown <mjbme10@yahoo.com>
2022-05-19 13:30:40 -0500

    Changed "region" to use variable

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "dev_public_subnet" {
   vpc_id                  = aws_vpc.dev_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-west-2a"
+  availability_zone       = "${var.region}a"
 
   tags = {
     Name = "dev_public"
--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "${var.region}"
   shared_credentials_files = ["credentials"]
   profile = "default"
 }
